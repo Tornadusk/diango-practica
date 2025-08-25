b9ac5af8f58e826eff49db4f8f9141f4b112c526
unknown <gokugangas@gmail.com>
2025-08-24 20:41:47 -0600

    Reorganizar rutas: index como página principal, home como segunda ruta

--- Sitio/urls.py
+++ Sitio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home),
-	path('index/', views.index, name='index')
+	path('', views.index, name='index'),
+	path('home/', views.home, name='home')
 ]
